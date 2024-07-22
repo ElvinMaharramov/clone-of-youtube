db9deaeda111fe81de15a9c59be5bf6611b6cb67
ElvinMaharramov <elvinmaharramov98@gmail.com>
2024-07-22 14:59:44 +0400

    useState removed in LikedVideosScreen.jsx

--- src/screens/likedVideosScreen/LikedVideosScreen.jsx
+++ src/screens/likedVideosScreen/LikedVideosScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Col, Container, Row } from 'react-bootstrap';
